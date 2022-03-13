2ed5a3e1f8b497c244da6aca0fbce54ebcacc772
Akisan <akisan.98@gmail.com>
2022-03-13 20:40:29 +0100

    Touch Ups

--- src/app/project-details.ts
+++ src/app/project-details.ts
@@ -46,13 +46,34 @@ export const FEATURED_PROJECTS: Project[] = [
 
 export const OTHER_PROJECTS: Project[] = [
     {
+        title: 'Portfolio Website',
+        platform: 'Web (Angular)',
+        tools: 'Angular • HTML • CSS • SCSS • TypeScript • Bootstrap 5 • JSON',
+        buttons: {
+            project: '#',
+        }
+    },
+    
+    {
+        title: 'Chin Chin',
+        platform: 'Android & iOS',
+        tools: 'Android • Flutter • iOS • Figma • RESTful API • JSON',
+        buttons: {
+            github: 'https://github.com/Akisan98/chin_chin',
+            googlePlay: 'https://play.google.com/store/apps/details?id=ml.akisan.party_bud',
+            // figma: 'Yes',
+            // external: 'Yes',
+            // project: 'Yes',
+        }
+    },
+    
+    {
         title: 'MeFit',
         platform: 'Web (React)',
-        tools: '.NET • C# • SQL Server • Azure • Docker • Entity Framework • ASP.NET • React • Redux • Material UI • RESTful API • JSON',
-        image: '/assets/mist.png',
+        tools: '.NET • C# • SQL Server • Azure • Docker • Entity Framework • ASP.NET • React • Redux • HTML • CSS • JSX • Material UI • RESTful API • JSON',
         buttons: {
             // github: 'Yes',
-            // gitlab: 'Yes',
+            gitlab: 'https://gitlab.com/Akisan98/MeFit',
             // googlePlay: 'Yes',
             // figma: 'Yes',
             // external: 'Yes',
@@ -63,11 +84,10 @@ export const OTHER_PROJECTS: Project[] = [
     {
         title: 'Pokédex',
         platform: 'Web (Angular)',
-        tools: 'Angular • RESTful API • JSON',
-        image: '/assets/mist.png',
+        tools: 'Angular • HTML • CSS • TS • Heroku • RESTful API • JSON',
         buttons: {
             // github: 'Yes',
-            // gitlab: 'https://young-forest-16786.herokuapp.com/',
+            gitlab: 'https://gitlab.com/Akisan98/pokeman-app',
             // googlePlay: 'Yes',
             // figma: 'Yes',
             external: 'https://young-forest-16786.herokuapp.com/',
@@ -79,36 +99,61 @@ export const OTHER_PROJECTS: Project[] = [
         title: 'Lost in Translation',
         platform: 'Web (React)',
         tools: 'React • HTML • CSS • JSX • Heroku • Vite • RESTful API • JSON',
-        image: '/assets/mist.png',
         buttons: {
             // github: 'Yes',
-            // gitlab: 'Yes',
+            gitlab: 'https://gitlab.com/Akisan98/assignment_6_react',
             // googlePlay: 'Yes',
             // figma: 'Yes',
-            // external: 'Yes',
+            external: 'https://lost-in-translation-zamfe.herokuapp.com/',
             // project: 'Yes',
         }
     },
 
     {
-        title: 'Chin Chin',
-        platform: 'Android & iOS',
-        tools: 'Android • Flutter • iOS • Figma • RESTful API • JSON',
-        image: '/assets/mist.png',
+        title: 'Trivia App',
+        platform: 'Web (Vue.js)',
+        tools: 'Vue.js • HTML • CSS • JS • Heroku • RESTful API • JSON',
         buttons: {
-            github: 'https://github.com/Akisan98/chin_chin',
-            googlePlay: 'https://play.google.com/store/apps/details?id=ml.akisan.party_bud',
+            // github: 'Yes',
+            gitlab: 'https://gitlab.com/Akisan98/assignment_5_vue',
+            // googlePlay: 'Yes',
             // figma: 'Yes',
-            // external: 'Yes',
+            external: 'https://aqueous-island-98671.herokuapp.com/',
             // project: 'Yes',
         }
     },
+
+    {
+        title: 'Movie Character API',
+        platform: 'Web (.NET)',
+        tools: '.NET • C# • SQL Server • Entity Framework • ASP.NET • Swagger',
+        buttons: {
+            gitlab: 'https://gitlab.com/Akisan98/MovieCharAPI_Assignment',
+        }
+    },
+
+    // {
+    //     title: 'Entity Framework',
+    //     platform: '.NET (Console)',
+    //     tools: '.NET • C# • SQL Server • Entity Framework',
+    //     buttons: {
+    //         gitlab: 'https://gitlab.com/Akisan98/sql_assignment',
+    //     }
+    // },
+
+    // {
+    //     title: '"RPG Game"',
+    //     platform: '.NET (Console)',
+    //     tools: '.NET • C#',
+    //     buttons: {
+    //         gitlab: 'https://gitlab.com/Akisan98/RPG-Assignment',
+    //     }
+    // },
 
     {
         title: 'My TV Shows',
         platform: 'Android',
         tools: 'Android • Android Studio • AndroidX • Room • RESTful API • JSON',
-        image: '/assets/mist.png',
         buttons: {
             // github: 'Yes',
             // googlePlay: 'Yes',
